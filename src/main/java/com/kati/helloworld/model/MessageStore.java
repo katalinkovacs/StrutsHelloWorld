f87b58977aee91682d9d0402ba418d3f32ed41f7
katalinkovacs <katalinkovacsnz@gmail.com>
2018-03-19 13:44:32 +1100

    some changes

--- src/main/java/com/kati/helloworld/model/MessageStore.java
+++ src/main/java/com/kati/helloworld/model/MessageStore.java
@@ -5,10 +5,14 @@ public class MessageStore {
 
 
     private String message;
+    private String message2;
+
 
     public MessageStore() {
 
         message = "Hello Kati! You are a Struts User";
+        message2 = "Hello Kati! This is the second msg";
+
     }
 
     public String getMessage()
@@ -16,4 +20,7 @@ public class MessageStore {
         return message;
     }
 
+    public String getMessage2() {
+        return message2;
+    }
 }
--- target/hello-world/HelloWorld.jsp
+++ target/hello-world/HelloWorld.jsp
@@ -8,5 +8,6 @@
   </head>
   <body>
     <h2><s:property value="messageStore.message" /></h2>
+    <h3><s:property value="messageStore.message2" /></h3>
   </body>
 </html>
